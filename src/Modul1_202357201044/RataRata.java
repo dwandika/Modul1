2195578f0e4fd16b1712731214324cb0e1c7ebe4
dwandika <mochamaddwiandika2@gmail.com>
2024-03-20 23:39:04 +0700

    first commit

--- src/Modul1_202357201044/RataRata.java
+++ src/Modul1_202357201044/RataRata.java
@@ -5,18 +5,18 @@ public class RataRata {
     public static void main(String[] args) {
         int data[][] = {{4, 6, 4, 2, 8, 4, 2, 10}, {4, 6, 4, 2, 8, 4, 2, 10}};
         int JumlahData = 0;
-        int JumlahElment = 0;
+        double JumlahElment = 0;
 
         System.out.println("Deret Angka :");
         for (int a = 0; a < data.length; a++) {
-            for (int b = 0; b < data[0].length; b++) {
+            for (int b = 0; b < data[a].length; b++) {
                 System.out.print(data[a][b] + " ");
                 JumlahData += data[a][b];
-                
+                JumlahElment++;
             }
             System.out.println();
         }
-        JumlahElment = data.length*data[0].length;
+
         double rataRATA = JumlahData / JumlahElment;
         System.out.println("Total Dari Array data \t\t: " + JumlahData);
         System.out.println("Jumlah Dari Elment Array data \t: " + JumlahElment);
